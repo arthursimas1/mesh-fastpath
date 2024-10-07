5c7015b116e562a61bb17e527602c7c2dfbd6e17
Arthur Simas <arthursimas1@gmail.com>
2024-10-07 20:01:45 -0300

    workload ping-echo: write datapoints to influxdb

--- workloads/ping-echo/sock_tcp_client.py
+++ workloads/ping-echo/sock_tcp_client.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import socket
 import time
 import signal
 
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
+
 assert len(sys.argv) == 2, 'wrong argument count. expected `python3 sock_client.py 127.0.0.1:8080`'
+
+url = os.environ['INFLUX_URL']
+token = os.environ['INFLUX_TOKEN']
+org = os.environ['INFLUX_ORG']
+bucket = os.environ['INFLUX_BUCKET']
+
+influx = influxdb_client.InfluxDBClient(
+  url=url,
+  token=token,
+  org=org,
+)
+
+influx_write_api = influx.write_api(write_options=SYNCHRONOUS)
 
 #[HOST, PORT] = '192.168.0.112:6300'.split(':')
 [HOST, PORT] = sys.argv[1].split(':')
@@ -39,12 +56,23 @@ while should_exit == 0:
   except Exception as e:
     print(e)
     break
+
   end = time.time_ns()
   diff = end - start
-  #print(data)
+
   print(f'{len(times)},{diff}')
+
+  datapoint = influxdb_client
+    .Point('latency')
+    .tag('workload', 'ping-echo')
+    .field('start', start)
+    .field('end', end)
+    .field('diff', diff)
+    .time(end, write_precision='ns')
+  write_api.write(org=org, bucket=bucket, record=[datapoint])
+
   times.append(diff)
-  time.sleep(.1)
+  time.sleep(.1 - diff * 1e+9)
 
 print('\nclosing socket')
 sock.close()
